6d4e01adbb7c0f0ba297f21aa0ce760f664d29e3
irisu-inwl <iris.dimension.eee@gmail.com>
2019-03-11 23:38:23 +0900

    first commit sec1_2

---
+++ Assets/Script/CameraRaycast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycast : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit, 1000)) {
+				// raycastに当たったオブジェクトの名前を取得
+                string objectName = hit.collider.gameObject.name;
+				// ロージアちゃんが得る
+                RosiaScript rosia =  GameObject.Find(objectName).GetComponent<RosiaScript>();
+                if (rosia != null){
+                    rosia.changeState();
+                }
+            }
+        }
+	}
+}
---
+++ Assets/Script/RosiaScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RosiaScript : MonoBehaviour {
+	private Animator anim;
+	private int state = 0;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetInteger("state", state);
+	}
+
+	public void changeState(){
+		state = (state + 1) % 2; //マジックナンバー…
+	}
+}
